f2da96706ea3b62570537739e5976f2d5bc712cc
y0b1 <auguzcht@gmail.com>
2025-06-08 12:10:43 +0800

    Revisions

--- flippify/main.py
+++ flippify/main.py
@@ -13,7 +13,7 @@ class FlippifyApp(tk.Tk):
         self.title("Flippify")
         self.geometry("1000x600")
         self.minsize(800, 1000)
-        self.iconbitmap(r"C:\Users\Yobi\Desktop\FlippifyApp\flippify\assets\Icon.ico")
+        self.iconbitmap(r"Icon.ico")
         
 
         sv_ttk.set_theme("dark")
